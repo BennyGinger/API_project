de606f4b333b7d6b3a30eaf4d5091e0e8770746f
Benoit Roux <benoit.roux@gmail.com>
2024-05-30 21:24:23 +0000

    finished the dictionnary

--- dictionnary/main.py
+++ dictionnary/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+# Load the thesaurus
+df = pd.read_csv("/home/API/dictionnary/data/dictionary.csv")
 
 @app.route("/")
 def home():
@@ -9,7 +12,18 @@ def home():
 
 @app.route("/api/v1/<word>")
 def definition(word: str):
-    return {'word': word, 'definition': word.upper()}
+    
+    # Get the definition of the word
+    matches = df.word.str.lower() == word.lower().replace("_", " ").replace("-", " ")
+    
+    # Check if the word is in the thesaurus
+    if matches.sum() == 0:
+        return {"error": f"Word {word} not found"}, 404
+    
+    # Get the definition
+    definition = df[matches].definition.iloc[0]
+    
+    return {'definition': definition,'word': word}
 
 if __name__ == "__main__":
     app.run(debug=True, port=5010)
